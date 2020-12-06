4efcc3781a72fe8ab0780cbadd761d16065cea1a
David Futcher <david@futcher.io>
2020-12-06 13:28:28 +0200

    Day 6 part 1 (Javascript)

---
+++ 6/part1.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+const groups = fs.readFileSync("input")
+                    .toString('utf-8')
+                    .split("\n\n")
+                    .map((l) => l.split("\n"));
+
+
+const flatten = xs => [].concat(...xs);
+const counts = groups.map((g) => new Set(flatten(g.map(flatten))).size)
+
+console.log(`Answer: ${counts.reduce((a,b) => a + b, 0)}`)
+
+module.exports = {groups}
