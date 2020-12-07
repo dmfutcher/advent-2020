e01ccdf0141c477f3a9b03a6e95ab4a5a429c55d
David Futcher <david@futcher.io>
2020-12-07 13:04:04 +0200

    Golf Day 7 down a little more

--- 7/part1.rb
+++ 7/part1.rb
@@ -3,18 +3,19 @@ require "set"
 @rules = Hash[File.open("input").readlines.map { |l|
             bag_colour, rest = l.chomp.split(" bags contain ")
             contains = rest.split(',').map { |x|
-                x == "no other bags." ? nil : [x.split(' ').slice(1,2).join(' ')] * Integer(x.split(' ')[0])}
+                n, *colours, _ = x.split(' ')
+                n == "no" ? [] : [colours.join(' ')] * Integer(n)
+            }
             [bag_colour, contains.flatten]
          }]
 
 def bag_children(type, coalesce=true)
-    direct = (coalesce ? Set.new(@rules[type]) : @rules[type]).reject { |b| b == nil }
+    direct = coalesce ? Array(Set.new(@rules[type])) : @rules[type]
     return direct + direct.map { |c| bag_children(c, coalesce) }.flatten
 end
 
 def can_hold(outer, inner)
-    children = bag_children(outer)
-    return children.include? inner
+    bag_children(outer).include? inner
 end
 
 if __FILE__ == $0
